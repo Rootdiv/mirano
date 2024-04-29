06050d3dabad5f02490a81d9968844ab9d6f55a8
Rootdiv <rootdiv9152@gmail.com>
2024-04-29 21:23:24 +0300

    feat: added header and section a hero

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   build: {
     rollupOptions: {
       output: {
