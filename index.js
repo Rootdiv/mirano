6a8c7cd1d4d026119cbc915af2553d97d00ad88d
Rootdiv <rootdiv9152@gmail.com>
2024-05-04 17:42:36 +0300

    feat: added functionality filters choices and added section the cart

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 import '@/scss/index.scss';
+
+const header = document.querySelector('header');
+const body = document.body;
+let headerHeight = header.offsetHeight;
+
+window.addEventListener('resize', () => {
+  headerHeight = header.offsetHeight;
+});
+
+window.addEventListener('scroll', () => {
+  const scrollDistance = window.scrollY;
+
+  if (scrollDistance > 200) {
+    header.classList.add('header_fixed');
+    body.style.paddingTop = `${headerHeight}px`;
+  } else {
+    header.classList.remove('header_fixed');
+    body.removeAttribute('style');
+  }
+});
+
+const adjustElementPosition = (element, count = 0) => {
+  const rect = element.getBoundingClientRect();
+  const viewportWidth = window.innerWidth;
+  if (rect.left < 0) {
+    element.style.left = '0';
+    element.style.right = 'auto';
+    element.style.transform = 'translateX(0)';
+  } else if (rect.right > viewportWidth) {
+    element.style.left = 'auto';
+    element.style.right = '0';
+    element.style.transform = 'translateX(0)';
+  } else {
+    element.removeAttribute('style');
+  }
+
+  const postRect = element.getBoundingClientRect();
+  if ((postRect.left < 0 || postRect.right > viewportWidth) && count < 3) {
+    count++;
+    adjustElementPosition(element, count);
+  }
+};
+
+const choices = document.querySelectorAll('.choices');
+
+choices.forEach(choice => {
+  const btn = choice.querySelector('.choices__btn');
+  const box = choice.querySelector('.choices__box');
+
+  btn.addEventListener('click', () => {
+    if (!btn.classList.contains('filters__select_active')) {
+      choices.forEach(choice => {
+        choice.querySelector('.choices__btn').classList.remove('filters__select_active');
+        choice.querySelector('.choices__box').classList.remove('choices__box_open');
+      });
+    }
+    btn.classList.toggle('filters__select_active');
+    box.classList.toggle('choices__box_open');
+
+    adjustElementPosition(box);
+
+    window.addEventListener('resize', () => {
+      adjustElementPosition(box);
+    });
+  });
+});
