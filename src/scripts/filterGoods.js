4ba7bed2454b9a9894cd72367d895c2c6710a4a4
Rootdiv <rootdiv9152@gmail.com>
2024-05-11 11:38:45 +0300

    refactor: updated filter the price

--- src/scripts/filterGoods.js
+++ src/scripts/filterGoods.js
@@ -1,39 +1,50 @@
 import { fetchGoods } from '@/scripts/API';
 import { debounce } from '@/scripts/debounce';
 
-const filterType = (type, prices) => {
-  fetchGoods({ type: type.value, ...prices });
-};
-
 export const filterGoods = () => {
   const filtersForm = document.querySelector('.filters__form');
   const goodsTitle = document.querySelector('.goods__title');
   filtersForm.reset();
 
-  const filtersPrice = {};
+  const applyFilters = () => {
+    const formData = new FormData(filtersForm);
 
-  filterType(filtersForm.type);
+    const type = formData.get('type');
+    const minPrice = formData.get('minPrice');
+    const maxPrice = formData.get('maxPrice');
+    const params = {};
 
-  const debouncedFilterType = debounce(() => {
-    filterType(filtersForm.type, filtersPrice);
-  }, 300);
+    if (type) {
+      params.type = type;
+    }
+    if (minPrice) {
+      params.minPrice = minPrice;
+    }
+    if (maxPrice) {
+      params.maxPrice = maxPrice;
+    }
+
+    fetchGoods(params);
+  };
+
+  applyFilters();
+
+  const applyPriceFilters = debounce(applyFilters, 300);
 
   filtersForm.addEventListener('input', ({ target }) => {
     if (target.name === 'type') {
       goodsTitle.textContent = target.labels[0].textContent;
       filtersForm.minPrice.value = '';
       filtersForm.maxPrice.value = '';
-      filterType(filtersForm.type);
+      applyFilters();
+      return;
     }
 
     if (target.name === 'minPrice' || target.name === 'maxPrice') {
       target.value = target.value.replace(/[^[0-9]/g, '');
       if (target.value) {
-        filtersPrice[target.name] = target.value;
-      } else {
-        delete filtersPrice[target.name];
+        applyPriceFilters();
       }
-      debouncedFilterType();
     }
   });
 };
